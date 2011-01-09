7ffc958a1d20a7034ce6d64bcb14818daac5edf0
Martin Baulig <martin@novell.com>
2011-01-09 18:14:11 +0100

    ARM Registers.

--- sysdeps/server/arm-arch.cpp
+++ sysdeps/server/arm-arch.cpp
@@ -1,5 +1,24 @@
 #include <arm-arch.h>
 
+#define INFERIOR_REG_R0(r)		r.regs.ARM_r0
+#define INFERIOR_REG_R1(r)		r.regs.ARM_r1
+#define INFERIOR_REG_R2(r)		r.regs.ARM_r2
+#define INFERIOR_REG_R3(r)		r.regs.ARM_r3
+#define INFERIOR_REG_R4(r)		r.regs.ARM_r4
+#define INFERIOR_REG_R5(r)		r.regs.ARM_r5
+#define INFERIOR_REG_R6(r)		r.regs.ARM_r6
+#define INFERIOR_REG_R7(r)		r.regs.ARM_r7
+#define INFERIOR_REG_R8(r)		r.regs.ARM_r8
+#define INFERIOR_REG_R9(r)		r.regs.ARM_r9
+#define INFERIOR_REG_R10(r)		r.regs.ARM_r10
+#define INFERIOR_REG_FP(r)		r.regs.ARM_fp
+#define INFERIOR_REG_IP(r)		r.regs.ARM_ip
+#define INFERIOR_REG_SP(r)		r.regs.ARM_sp
+#define INFERIOR_REG_LR(r)		r.regs.ARM_lr
+#define INFERIOR_REG_PC(r)		r.regs.ARM_pc
+#define INFERIOR_REG_CPSR(r)		r.regs.ARM_cpsr
+#define INFERIOR_REG_ORIG_R0(r)		r.regs.ARM_ORIG_r0
+
 MdbArch *
 mdb_arch_new (MdbInferior *inferior)
 {
@@ -27,7 +46,17 @@ ArmArch::ChildStopped (int stopsig, bool *out_remain_stopped)
 ErrorCode
 ArmArch::GetFrame (StackFrame *out_frame)
 {
-	return ERR_NOT_IMPLEMENTED;
+	ErrorCode result;
+
+	result = GetRegisters ();
+	if (result)
+		return result;
+
+	out_frame->address = (gsize) INFERIOR_REG_PC (current_regs);
+	out_frame->stack_pointer = (gsize) INFERIOR_REG_SP (current_regs);
+	out_frame->frame_address = (gsize) INFERIOR_REG_FP (current_regs);
+
+	return ERR_NONE;
 }
 
 void
@@ -37,19 +66,63 @@ ArmArch::RemoveBreakpointsFromTargetMemory (guint64 start, guint32 size, gpointe
 int
 ArmArch::GetRegisterCount (void)
 {
-	return 0;
+	return DEBUGGER_REG_LAST;
 }
 
 ErrorCode
 ArmArch::GetRegisterValues (guint64 *values)
 {
-	return ERR_NOT_IMPLEMENTED;
+	ErrorCode result;
+
+	result = GetRegisters ();
+	if (result)
+		return result;
+
+	values [DEBUGGER_REG_R0] = INFERIOR_REG_R0 (current_regs);
+	values [DEBUGGER_REG_R1] = INFERIOR_REG_R1 (current_regs);
+	values [DEBUGGER_REG_R2] = INFERIOR_REG_R2 (current_regs);
+	values [DEBUGGER_REG_R3] = INFERIOR_REG_R3 (current_regs);
+	values [DEBUGGER_REG_R4] = INFERIOR_REG_R4 (current_regs);
+	values [DEBUGGER_REG_R5] = INFERIOR_REG_R5 (current_regs);
+	values [DEBUGGER_REG_R6] = INFERIOR_REG_R6 (current_regs);
+	values [DEBUGGER_REG_R7] = INFERIOR_REG_R7 (current_regs);
+	values [DEBUGGER_REG_R8] = INFERIOR_REG_R8 (current_regs);
+	values [DEBUGGER_REG_R9] = INFERIOR_REG_R9 (current_regs);
+	values [DEBUGGER_REG_R10] = INFERIOR_REG_R10 (current_regs);
+	values [DEBUGGER_REG_FP] = INFERIOR_REG_FP (current_regs);
+	values [DEBUGGER_REG_IP] = INFERIOR_REG_IP (current_regs);
+	values [DEBUGGER_REG_SP] = INFERIOR_REG_SP (current_regs);
+	values [DEBUGGER_REG_LR] = INFERIOR_REG_LR (current_regs);
+	values [DEBUGGER_REG_PC] = INFERIOR_REG_PC (current_regs);
+	values [DEBUGGER_REG_CPSR] = INFERIOR_REG_CPSR (current_regs);
+	values [DEBUGGER_REG_ORIG_R0] = INFERIOR_REG_ORIG_R0 (current_regs);
+
+	return ERR_NONE;
 }
 
 ErrorCode
 ArmArch::SetRegisterValues (const guint64 *values)
 {
-	return ERR_NOT_IMPLEMENTED;
+	INFERIOR_REG_R0 (current_regs) = values [DEBUGGER_REG_R0];
+	INFERIOR_REG_R1 (current_regs) = values [DEBUGGER_REG_R1];
+	INFERIOR_REG_R2 (current_regs) = values [DEBUGGER_REG_R2];
+	INFERIOR_REG_R3 (current_regs) = values [DEBUGGER_REG_R3];
+	INFERIOR_REG_R4 (current_regs) = values [DEBUGGER_REG_R4];
+	INFERIOR_REG_R5 (current_regs) = values [DEBUGGER_REG_R5];
+	INFERIOR_REG_R6 (current_regs) = values [DEBUGGER_REG_R6];
+	INFERIOR_REG_R7 (current_regs) = values [DEBUGGER_REG_R7];
+	INFERIOR_REG_R8 (current_regs) = values [DEBUGGER_REG_R8];
+	INFERIOR_REG_R9 (current_regs) = values [DEBUGGER_REG_R9];
+	INFERIOR_REG_R10 (current_regs) = values [DEBUGGER_REG_R10];
+	INFERIOR_REG_FP (current_regs) = values [DEBUGGER_REG_FP];
+	INFERIOR_REG_IP (current_regs) = values [DEBUGGER_REG_IP];
+	INFERIOR_REG_SP (current_regs) = values [DEBUGGER_REG_SP];
+	INFERIOR_REG_LR (current_regs) = values [DEBUGGER_REG_LR];
+	INFERIOR_REG_PC (current_regs) = values [DEBUGGER_REG_PC];
+	INFERIOR_REG_CPSR (current_regs) = values [DEBUGGER_REG_CPSR];
+	INFERIOR_REG_ORIG_R0 (current_regs) = values [DEBUGGER_REG_ORIG_R0];
+
+	return SetRegisters ();
 }
 
 ErrorCode
